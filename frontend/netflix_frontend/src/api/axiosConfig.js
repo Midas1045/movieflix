1ce9fa852faad58a23204fb8790634d2eb36d226
Midas1045 <obinnaclinton85@gmail.com>
2026-04-07 20:18:48 +0100

    edited axiosConfig.js

--- frontend/netflix_frontend/src/api/axiosConfig.js
+++ frontend/netflix_frontend/src/api/axiosConfig.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://{your-backend-url}/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    baseURL:'https://http://13.220.45.156:8080//api',
 });
